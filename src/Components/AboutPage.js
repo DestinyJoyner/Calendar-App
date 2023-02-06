a245a285d106359c6f6b72196de2a8dae6fc0980
DestinyJoyner <destinyjoyner@pursuit.org>
2023-02-06 00:38:46 -0500

    added links to about me page

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -1,3 +1,6 @@
+import { BsGithub } from "react-icons/bs"
+import{ AiOutlineLinkedin } from "react-icons/ai"
+import { GoMail } from "react-icons"
 import "./AboutPage.css"
 
 function AboutPage(props) {
@@ -5,6 +8,19 @@ function AboutPage(props) {
         <div className="about">
             <section className="about-info">
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </section>
+            <section className="about-links">
+                <a href ="https://github.com/DestinyJoyner" >
+                    <BsGithub
+                    color={"#F9FBFF"}
+                    size={"40px"} />
+                </a>
+                <a href ="https://www.linkedin.com/in/destiny-joyner-934846243/" >
+                    <AiOutlineLinkedin
+                    color={"#F9FBFF"}
+                    size={"40px"} />
+                </a>
+               <span></span>
             </section>
         </div>
     );
