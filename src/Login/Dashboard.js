bbfcaa5a4bada2406e6341cdc94654e77157b57d
DestinyJoyner <destinyjoyner@pursuit.org>
2023-02-07 12:17:54 -0500

    set up login with axios call to backend

---
+++ src/Login/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard(props) {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ src/Login/Preferences.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Preferences(props) {
+    return (
+        <div>
+            <h2>Preferences</h2>
+        </div>
+    );
+}
+
+export default Preferences;
